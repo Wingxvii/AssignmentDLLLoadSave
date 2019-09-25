eaa9ccfa9b76915379e6aefa62ccd81959455e3e
John <john.wang@uoit.net>
2019-09-25 16:35:30 -0400

    added all the code and separated the dll code

--- Assets/Scripts/SaveMove.cs
+++ Assets/Scripts/SaveMove.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using UnityEngine.UI;
 using UnityEngine;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -8,10 +7,13 @@ using System.Collections.Generic;
 
 public class SaveMove : MonoBehaviour
 {
-    [DllImport("CNET.dll")]
+    [DllImport("SAVER.dll")]
     static extern void SavePosition(float px, float py, float pz);
-    [DllImport("CNET.dll")]
-    static extern void GetPosition(Action<float, float, float> action);
+    [DllImport("SAVER.dll")]
+    static extern void RequestPosition();
+    [DllImport("SAVER.dll")]
+    static extern void SetupRecieve(Action<float, float, float> action);
+
 
     private Transform playerTransform;
     private Rigidbody playerBody;
@@ -23,30 +25,41 @@ public class SaveMove : MonoBehaviour
     {
         playerTransform = this.GetComponent<Transform>();
         playerBody = this.GetComponent<Rigidbody>();
+        SetupRecieve(RecievePosition);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W)) {
+        if (Input.GetKey(KeyCode.W)) {
             playerBody.velocity += new Vector3(1 * -moveSpeed,0,0);
         }
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
         {
             playerBody.velocity += new Vector3(1 * moveSpeed, 0, 0);
         }
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             playerBody.velocity += new Vector3(0, 0,1 * -moveSpeed);
         }
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
-            playerBody.velocity += new Vector3(0,0,-1 * moveSpeed);
+            playerBody.velocity += new Vector3(0,0,1 * moveSpeed);
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            RequestPosition();
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            SavePosition(playerTransform.position.x, playerTransform.position.y, playerTransform.position.z );
         }
 
     }
 
     void RecievePosition(float px, float py, float pz) {
-
+        Debug.Log("Dot Cat");
+        Vector3 tempPos = new Vector3(px, py, pz);
+        playerTransform.position = tempPos;
     }
 }
